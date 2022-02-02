8b5b0017015a028a003fbfd6788918827da38dba
Piotr Gnybek <mewgamp@gmail.com>
2022-02-02 20:12:42 +0100

    Add login functionality

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class RootPage extends StatelessWidget {
         builder: (context, snapshot) {
           final user = snapshot.data;
           if (user == null) {
-            return const LoginPage();
+            return LoginPage();
           }
           return HomePage(user: user);
         });
