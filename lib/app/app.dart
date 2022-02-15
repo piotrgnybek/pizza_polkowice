5951c217b6f51abb591bfbce90625281607de0e4
Piotr Gnybek <mewgamp@gmail.com>
2022-02-15 19:59:27 +0100

    Move my App to app.dart

---
+++ lib/app/app.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:pizza_polkowice/app/features/home/home_page.dart';
+import 'package:pizza_polkowice/app/features/login/login_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          final user = snapshot.data;
+          if (user == null) {
+            return LoginPage();
+          }
+          return HomePage(user: user);
+        });
+  }
+}
