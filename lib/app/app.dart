30038adedbcceea55fa2da3654538b5f9be8ba39
Piotr Gnybek <mewgamp@gmail.com>
2022-02-15 22:16:10 +0100

    Add root page cubit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_polkowice/app/cubit/root_cubit.dart';
 import 'package:pizza_polkowice/app/features/home/home_page.dart';
 import 'package:pizza_polkowice/app/features/login/login_page.dart';
 
@@ -21,14 +22,31 @@ class RootPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          final user = snapshot.data;
+    return BlocProvider(
+      create: (context) => RootCubit()..start(),
+      child: BlocBuilder<RootCubit, RootState>(
+        builder: (context, state) {
+          if (state.errorMessage.isNotEmpty) {
+            return Center(
+              child: Text(
+                'Something went wrong : ${state.errorMessage}',
+              ),
+            );
+          }
+
+          if (state.isLoading) {
+            //== true
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final user = state.user;
           if (user == null) {
             return LoginPage();
           }
           return HomePage(user: user);
-        });
+        },
+      ),
+    );
   }
 }
