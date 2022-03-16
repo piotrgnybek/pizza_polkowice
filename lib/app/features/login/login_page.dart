1c730b321055b0e8e8c94e927d5ccd83bf748860
piotrgnybek <99535922+piotrgnybek@users.noreply.github.com>
2022-03-17 00:07:05 +0100

    Add bloc to login_page

--- lib/app/features/login/login_page.dart
+++ lib/app/features/login/login_page.dart
@@ -1,108 +1,116 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_polkowice/app/features/login/cubit/login_cubit.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({
     Key? key,
   }) : super(key: key);
-  final emailController = TextEditingController();
-
-  final passwordController = TextEditingController();
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
-var errorMessage = '';
-var isCreatingAccount = false;
+final email = TextEditingController();
+final password = TextEditingController();
 
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(isCreatingAccount == true
-                  ? 'Zarejestruj się'
-                  : 'Zaloguj się'),
-              const SizedBox(height: 20),
-              TextField(
-                controller: widget.emailController,
-                decoration: const InputDecoration(hintText: 'E-mail'),
-              ),
-              TextField(
-                controller: widget.passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(hintText: 'Password'),
-              ),
-              const SizedBox(height: 20),
-              Text(errorMessage),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  if (isCreatingAccount == true) {
-                    // rejestracja
-                    try {
-                      await FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
-                        email: widget.emailController.text,
-                        password: widget.passwordController.text,
-                      );
-                    } catch (error) {
-                      setState(
-                        () {
-                          errorMessage = error.toString();
-                        },
-                      );
-                    }
-                  } else {
-                    // logowanie
-                    try {
-                      await FirebaseAuth.instance.signInWithEmailAndPassword(
-                        email: widget.emailController.text,
-                        password: widget.passwordController.text,
-                      );
-                    } catch (error) {
-                      setState(
-                        () {
-                          errorMessage = error.toString();
-                        },
-                      );
-                    }
-                  }
-                },
-                child: Text(isCreatingAccount == true
-                    ? 'Zarejestruj się'
-                    : 'Zaloguj się'),
-              ),
-              const SizedBox(height: 20),
-              if (isCreatingAccount == false) ...[
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      isCreatingAccount = true;
-                    });
-                  },
-                  child: const Text('Utwórz konto'),
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: BlocBuilder<LoginCubit, LoginState>(
+        builder: (context, state) {
+          return Scaffold(
+            body: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(state.isCreatingAccount
+                        ? 'Zarejestruj się'
+                        : 'Zaloguj się'),
+                    const SizedBox(height: 20),
+                    TextField(
+                      controller: email,
+                      decoration: const InputDecoration(hintText: 'E-mail'),
+                    ),
+                    TextField(
+                      controller: password,
+                      obscureText: true,
+                      decoration: const InputDecoration(hintText: 'Password'),
+                    ),
+                    const SizedBox(height: 20),
+                    Text('${state.errorMessage}'),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (state.isCreatingAccount) {
+                          // rejestracja
+                          try {
+                            context
+                                .read<LoginCubit>()
+                                .signUp(email.text, password.text);
+                          } catch (error) {
+                            Center(
+                              child: Text(
+                                'Something went wrong : ${state.errorMessage}',
+                              ),
+                            );
+                          }
+                        } else {
+                          // logowanie
+                          try {
+                            context
+                                .read<LoginCubit>()
+                                .signIn(email.text, password.text);
+                          } catch (error) {
+                            Center(
+                              child: Text(
+                                'Something went wrong : ${state.errorMessage}',
+                              ),
+                            );
+                          }
+                        }
+                      },
+                      child: Text(state.isCreatingAccount
+                          ? 'Zarejestruj się'
+                          : 'Zaloguj się'),
+                    ),
+                    const SizedBox(height: 20),
+
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(state.isCreatingAccount
+                          ? 'Utwórz konto'
+                          : 'Masz już konto?'),
+                    ),
+                    // if (state.isCreatingAccount) {
+                    //   return TextButton(
+                    //      onPressed: () {
+
+                    //      },
+                    //     child: Text('Utwórz konto'),
+                    //   );
+                    // }
+
+                    // if (isCreatingAccount == true) ...[
+                    //   TextButton(
+                    //     onPressed: () {
+                    //       setState(() {
+                    //         isCreatingAccount = false;
+                    //       });
+                    //     },
+                    //     child: const Text('Masz już konto?'),
+                    //   ),
+                    // ],
+                  ],
                 ),
-              ],
-              if (isCreatingAccount == true) ...[
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      isCreatingAccount = false;
-                    });
-                  },
-                  child: const Text('Masz już konto?'),
-                ),
-              ],
-            ],
-          ),
-        ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
